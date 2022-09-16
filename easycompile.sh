5cceb42460bc52bbbc35286ec01dbcdc9780335e
Gatien-Oudoire <naguite.ytb@gmail.com>
2022-09-16 20:05:51 +0200

    Simplification des ecritures en couleurs et plusieurs fichier peuvent etre passes

--- easycompile.sh
+++ easycompile.sh
@@ -2,9 +2,17 @@
 
 fileName=$1
 
+afficherRouge () {
+    echo -e "\e[31m$1 \e[0m"
+}
+
+afficherVert () {
+    echo -e "\e[32m$1 \e[0m"
+}
+
 if [ $# -eq 0 ]
     then 
-        echo "Vous devez entrer un nom de fichier comme argument de commande."
+        afficherRouge "Vous devez entrer un nom de fichier comme argument de commande."
         exit
     fi
 
@@ -16,23 +24,20 @@ if [[ $fileName == *.c ]]
 
 if ! [[ -f "$fileName.c" ]]
     then
-        echo "Ce fichier n'existe pas dans votre répértoire."
+        afficherRouge "Ce fichier n'existe pas dans votre répértoire."
         exit 
     fi
 
 gcc -Wall -Wextra -std=c99 -Wpedantic -g -fsanitize=address,undefined \
-$fileName.c -o $fileName
+    "$@" -o "$fileName"
 
 if ! [ $? -eq 0 ]
     then
-        echo -e "\e[31m" # permet d afficher le texte en rouge
-        echo -e "Echec de la compilation"
+        afficherRouge "Echec de la compilation"
         exit
     fi
 
-echo -e "\e[32m"
-echo -e "Compilation terminée, pour lancer le programme \
+afficherVert "Compilation terminée, pour lancer le programme \
 taper \"./$fileName\" dans votre terminal."
 
 
-echo -e "\e[0m"
